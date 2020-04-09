0d49d4c5edf1af2983bbef9f94e48adc77a28c27
LeticiaLCavalcanti <wmli515@gmail.coma>
2020-04-09 04:19:27 -0300

    authentication with jsonwebtoken

--- Routes/users.js
+++ Routes/users.js
@@ -2,6 +2,14 @@ const express = require('express');
 const router = express.Router();
 const Users = require('../model/user');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+// auxiliary functions
+    const createUserToken = (userId) => {
+        return jwt.sign({id: userId}, '13dnms', { expiresIn: '7d'});
+    };
+//
+
 
 router.get('/', async (req, res) => {
    try {
@@ -22,7 +30,7 @@ router.post('/create', async (req, res) => {
 
        const user = await Users.create(req.body);
        user.password = undefined;
-       return res.send(user);
+       return res.send({user, token: createUserToken(user.id)});
    }
    catch (err) {
        return res.send({error: 'Erro ao buscar usuário!'})
@@ -43,7 +51,7 @@ router.post('/auth', async (req, res) => {
        if(!pass_ok) return res.send({ error: 'Erro ao autenticar usuário!'});
 
        user.password = undefined;
-       return res.send(user);
+       return res.send({user, token: createUserToken(user.id)});
 
    }
    catch (err) {
